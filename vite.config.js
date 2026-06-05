e885561e991aed02a89448e5eaf557717fe462aa
merargroup-dental <merar.group@gmail.com>
2026-06-04 23:12:00 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        app: resolve(__dirname, 'app.html'),
-      }
-    }
-  }
 })
